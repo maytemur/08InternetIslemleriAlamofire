5aec78d737cd31d9d004852fb792d2388105b18f
Mustafa Aytemür <maytemur@gmail.com>
2023-05-05 16:38:46 +0300

    Initial Commit

---
+++ 08InternetIslemleriAlamofire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  08InternetIslemleriAlamofire
+//
+//  Created by maytemur on 5.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
